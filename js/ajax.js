9f19881226ff40f7fc71d083a5212113245fbf16
altunbas-huseyin <altunbas.huseyin@gmail.com>
2013-07-16 16:38:50 +0300

    deneme
    deneme

---
+++ js/ajax.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(document).bind('deviceready', function(){
+        //Phonegap ready
+        onDeviceReady();
+    });
+
+    var output = $('#output');
+    var url = 'http://lifeafterracing.ustrotting.com/landmarks.cfc?method=remoteGetLandMarks&callback=?';
+	
+	$.ajax({
+		type: 'GET',
+		url: url,
+		jsonpCallback: 'jsonCallback',
+		contentType: "application/json",
+		dataType: 'jsonp',
+		success: function(data) {
+			$.each(data, function(i,item){ 
+                var landmark = '<h1>'+item.NAME+'</h1>'
+                + '<p>'+item.LATITUDE+'<br>'
+                + item.LONGITUDE+'</p>';
+                output.append(landmark);
+			});
+		},
+		error: function(e) {
+			console.log(e.message);
+		}
+	});
+});
