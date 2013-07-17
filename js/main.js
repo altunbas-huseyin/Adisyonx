b679f84957691156c989df515cbf762eea59e74e
altunbas-huseyin <altunbas.huseyin@gmail.com>
2013-07-17 17:43:11 +0300

    Revert "1"
    This reverts commit 2bad5a3e0a676957c5b8137d43552273bdaa8aab.

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $(document).bind('deviceready', function(){
+        //Phonegap ready
+        onDeviceReady();
+    });
+
+    var output = $('#output');
+    var url = 'http://lifeafterracing.ustrotting.com/landmarks.cfc?method=remoteGetLandMarks&callback=?';
+	$.ajax({
+		type: 'GET',
+		url: url,
+		jsonpCallback: 'jsonCallback',
+		contentType: "application/json",
+		dataType: 'jsonp',
+		success: function(data) {
+			alert(data);
+			$.each(data, function(i,item){ 
+                var landmark = '<h1>'+item.NAME+'</h1>'
+                + '<p>'+item.LATITUDE+'<br>'
+                + item.LONGITUDE+'</p>';
+                output.append(landmark);
+			});
+		},
+		error: function(e) {
+			console.log(e.message);
+		}
+	});
+});
+
+
+
+function alert_yap(isim)
+{
+	alert(isim)
+}
